710eed055124387234fb59e4fa2d1791081e5916
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-01-31 16:23:26 +0000

    Added JastAddJ

---
+++ MJ/JastAddJ/Java1.4Backend/JavaCompiler.java
@@ -0,0 +1,29 @@
+import AST.*;
+
+class JavaCompiler extends Frontend {
+  public static void main(String args[]) {
+    if(!compile(args))
+      System.exit(1);
+  }
+  
+  public static boolean compile(String args[]) {
+    return new JavaCompiler().process(
+        args,
+        new BytecodeParser(),
+        new JavaParser() {
+          public CompilationUnit parse(java.io.InputStream is, String fileName) throws java.io.IOException, beaver.Parser.Exception {
+            return new parser.JavaParser().parse(is, fileName);
+          }
+        }
+    );
+  }
+  protected void processNoErrors(CompilationUnit unit) {
+    unit.java2Transformation();
+    if(Program.hasOption("-print"))
+      System.out.println(unit);
+    unit.generateClassfile();
+  }
+
+  protected String name() { return "Java1.4Frontend + Backend"; }
+  protected String version() { return "R20071015"; }
+}
