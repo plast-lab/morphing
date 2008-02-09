5e8f46dba28a3ebbd1b7c953c5ed529887f87d9a
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-02-09 07:20:04 +0000

    First version of backend. Very minimal expansion.

--- MJ/MJBackend/src/MJCompiler.java
+++ MJ/MJBackend/src/MJCompiler.java
@@ -1,27 +1,46 @@
 import AST.*;
 
 class MJCompiler extends Frontend {
-  public static void main(String args[]) {
-    if(!compile(args))
-      System.exit(1);
-  }
+	public static void main(String args[]) {
+		if (!compile(args))
+			System.exit(1);
+	}
 
-  public static boolean compile(String args[]) {
-    return new MJCompiler().process(
-        args,
-        new BytecodeParser(),
-        new JavaParser() {
-          public CompilationUnit parse(java.io.InputStream is, String fileName) throws java.io.IOException, beaver.Parser.Exception {
-            return new parser.JavaParser().parse(is, fileName);
-          }
-        }
-    );
-  }
-  protected void processNoErrors(CompilationUnit unit) {
-    unit.java2Transformation();
-    unit.generateClassfile();
-  }
+	static java.util.Set expansionTypes = new java.util.HashSet();
 
-  protected String name() { return "MJCompiler"; }
-  protected String version() { return "R20080124"; }
+	public static boolean compile(String args[]) {
+		MJCompiler compiler = new MJCompiler();
+		boolean b = compiler.process(args, new BytecodeParser(),
+				new JavaParser() {
+					public CompilationUnit parse(java.io.InputStream is,
+							String fileName) throws java.io.IOException,
+							beaver.Parser.Exception {
+						return new parser.JavaParser().parse(is, fileName);
+					}
+				});
+		if (b) {
+			// generate the expanded classes.
+			for (java.util.Iterator etIt = expansionTypes.iterator(); etIt
+					.hasNext();) {
+				TypeDecl t = (TypeDecl) etIt.next();
+				t.generateClassfile();
+				t.clear();
+			}
+		}
+		return b;
+	}
+
+	protected void processNoErrors(CompilationUnit unit) {
+		expansionTypes.addAll(unit.collectExpansionTypes());
+		unit.java2Transformation();
+		unit.generateClassfile();
+	}
+
+	protected String name() {
+		return "MJCompiler";
+	}
+
+	protected String version() {
+		return "R20080124";
+	}
 }
