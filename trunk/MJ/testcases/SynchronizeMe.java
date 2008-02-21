6130711fe00e9559704ef50662c8cb33a2f1263b
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-02-21 00:18:34 +0000

    modified test file with vector type variables

--- trunk/MJ/testcases/SynchronizeMe.java
+++ trunk/MJ/testcases/SynchronizeMe.java
@@ -11,15 +11,28 @@ public class SynchronizeMe<interface T> implements T {
 	mutex = this;
     }
 
-    <R>[m] for (R m () : T.methods) 
+
+    <R,A*>[m] for ( !final R m (A) : T.methods) 
+    public R m (A args) {
+	return me.m(args);
+    }
+
+    /*
+    <R>[m] for ( !final R m () : T.methods )
     public R m () {
 	return me.m();
     }
+    */
 
     public static void main (String[] argv) {
 	List myList = new ArrayList();
 	SynchronizeMe<List> synchronizedList = 
 	    new SynchronizeMe<List>(myList);
+	myList.add("a");
+	myList.add("b");
 	System.out.println("size:" + synchronizedList.size());
+
+	List sublist = synchronizedList.subList(0,1);
+	System.out.println("sublist size: " + sublist.size());
     }
 }
