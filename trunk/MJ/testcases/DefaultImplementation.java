267611dad1943fcb9336ad3f750b876cd3ec38ba
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-03-16 02:14:19 +0000

    DefaultImplementation along with BasicTest2 works

--- trunk/MJ/testcases/DefaultImplementation.java
+++ trunk/MJ/testcases/DefaultImplementation.java
@@ -6,48 +6,47 @@ public class DefaultImplementation<X, interface I> implements I {
 
     // for all methods in I, if the same method does
     // not appear in X, provide default implementation.
-    <R extends Object,A*>[m]for( R m (A) : I.methods ; no R m (A) : X.methods )
-    public R m (A a) { return null; }
+    <R extends Object,A*,E*>[m]for( R m (A) throws E: I.methods ; no R m (A) : X.methods )
+    public R m (A a) throws E { return null; }
 
-    <A1*>[m]for( void m (A1) : I.methods ; no void m (A1) : X.methods )
-    public void m (A1 a) { }
+    <A1*,E*>[m]for( void m (A1) throws E : I.methods ; no void m (A1) : X.methods )
+    public void m (A1 a) throws E { }
 
-    <A2*>[m]for( boolean m (A2) : I.methods ; no boolean m (A2) : X.methods )
-    boolean m (A2 a) { return false; }
+    <A2*,E*>[m]for( boolean m (A2) throws E : I.methods ; no boolean m (A2) : X.methods )
+    public boolean m (A2 a) throws E { return false; }
 
-    <A3*>[m]for( int m (A3) : I.methods ; no int m (A3) : X.methods )
-    public int m (A3 a) { return 0; }
+    <A3*,E*>[m]for( int m (A3) throws E : I.methods ; no int m (A3) : X.methods )
+    public int m (A3 a) throws E { return 0; }
 
-    <A*>[m]for( byte m (A) : I.methods ; no byte m (A) : X.methods )
-    byte m (A a) { return 0; }
+    <A*,E*>[m]for( byte m (A) throws E : I.methods ; no byte m (A) : X.methods )
+    byte m (A a) throws E { return 0; }
 
-    <A*>[m]for( short m (A) : I.methods ; no short m (A) : X.methods )
-    short m (A a) { return 0; }
+    <A*,E*>[m]for( short m (A) throws E : I.methods ; no short m (A) : X.methods )
+    short m (A a) throws E { return 0; }
 
-    <A*>[m]for( long m (A) : I.methods ; no long m (A) : X.methods )
-    long m (A a) { return 0; }
+    <A*,E*>[m]for( long m (A) throws E : I.methods ; no long m (A) : X.methods )
+    long m (A a) throws E { return 0; }
 
-    <A*>[m]for( char m (A) : I.methods ; no char m (A) : X.methods )
-    char m (A a) { return 0; }
+    <A*,E*>[m]for( char m (A) throws E : I.methods ; no char m (A) : X.methods )
+    char m (A a) throws E { return 0; }
 
-    <A*>[m]for( float m (A) : I.methods ; no float m (A) : X.methods )
-    float m (A a) { return 0; }
+    <A*,E*>[m]for( float m (A) throws E : I.methods ; no float m (A) : X.methods )
+    float m (A a) throws E { return 0; }
 
-    <A*>[m]for( double m (A) : I.methods ; no double m (A) : X.methods )
-    double m (A a) { return 0; }
+    <A*,E*>[m]for( double m (A) throws E : I.methods ; no double m (A) : X.methods )
+    double m (A a) throws E { return 0; }
 
     // for all methods in X that *do* correctly override
     // methods in I, we need to copy them. 
-    <R,A*>[m]for( R m (A) : I.methods ; some R m (A) : X.methods )
-    public R m (A a) { return x.m(a); }
+    <R,A*,E*>[m]for( !final R m (A) throws E : I.methods ; some R m (A) : X.methods )
+    public R m (A a) throws E { return x.m(a); }
 
-
-    <A*>[m]for( void m (A) : I.methods ; some void m (A) : X.methods )
-    public void m (A a) { x.m(a); }
+    <A*,E*>[m]for( !final void m (A) throws E : I.methods ; some void m (A) : X.methods )
+    public void m (A a) throws E { x.m(a); }
 
     // for all methods in X, such that there is no method
     // in I with the same name and arguments, copy method.
     <R,A*>[m]for( R m (A) : X.methods; no m (A) : I.methods)
-    R m (A a) { return x.m(a); }
+    public R m (A a) { return x.m(a); }
 
 }
--- trunk/MJ/testcases/junit/BasicTest2.java
+++ trunk/MJ/testcases/junit/BasicTest2.java
@@ -10,20 +10,92 @@ public class BasicTest2 extends TestCase {
     @Test public void testDefaultImplementation() {
 	IncompleteList il = new IncompleteList();
 
-	DefaultImplementation<IncompleteList, List> l =
+	DefaultImplementation<IncompleteList, List> l = 
 	    new DefaultImplementation<IncompleteList, List>(il);
 
-	// calling remove(int) should be fine.
-	l.remove(0);
-
 	l.clear(); // OK.
+
+	assertTrue(7 == l.size());
     }
+
+    @Test public void testDefaultImplementation2() {
+	IncompleteList il = new IncompleteList();
+
+	DefaultImplementation<IncompleteList, List> l = 
+	    new DefaultImplementation<IncompleteList, List>(il);
+
+	assertTrue(l.foo().equals("foo"));
+    }
+
+    @Test public void testDefaultImplementation3() {
+	IncompleteList il = new IncompleteList();
+
+	DefaultImplementation<IncompleteList, List> l = 
+	    new DefaultImplementation<IncompleteList, List>(il);
+	assertTrue(l.iterator() == null);
+    }
+
+    @Test public void testDefaultImplementation4() {
+	IncompleteList il = new IncompleteList();
+
+	DefaultImplementation<IncompleteList, List> l = 
+	    new DefaultImplementation<IncompleteList, List>(il);
+
+	assertTrue(l.remove(null) == false);
+    }
+
+    @Test public void testDefaultImplementation5() {
+	IncompleteList il = new IncompleteList();
+
+	DefaultImplementation<IncompleteList, List> l = 
+	    new DefaultImplementation<IncompleteList, List>(il);
+
+	assertTrue(l.indexOf(null) == 0);
+    }
+
 }
 
 class IncompleteList {
     // only implement clear() and size()
     public void clear() {}
     public int size() { return 7; }
-
     public String foo() { return "foo"; }
 }
+
+
+/*
+public class DefaultImplementationOfIncompleteList implements List {
+    IncompleteList x;
+    public DefaultImplementationOfIncompleteList(IncompleteList x) {
+	this.x = x;
+    }
+
+    public void clear() {}
+    public int size() { return 7; }
+    public String foo() { return "foo"; }
+
+    public boolean addAll(java.util.Collection c ) { return false; }
+    //    public int size() { return 0; }
+    public Iterator iterator() { return null; }
+    public boolean add(java.lang.Object o) { return false; }
+    public boolean containsAll(java.util.Collection c) { return false; }
+    public boolean retainAll(java.util.Collection c) { return false; }
+    //    public void clear() { }
+    public List subList(int i, int j) { return null; }
+    public boolean addAll(int i, java.util.Collection c) { return false; }
+    public Object[] toArray(java.lang.Object[] a ) { return null; }
+    public Object get(int i) { return null; }
+    public int indexOf(java.lang.Object a) { return 0; }
+    public int lastIndexOf(java.lang.Object a) { return 0; }
+    public ListIterator listIterator(int i) { return null;}
+    public Object set(int i, java.lang.Object o) { return null; }
+    public void add(int i, java.lang.Object o) { }
+    public Object[] toArray() { return null; } 
+    public boolean contains(java.lang.Object o) { return false; }
+    public Object remove(int i) { return null; }
+    public ListIterator listIterator() { return null; }
+    public boolean isEmpty() { return false; }
+    public boolean remove(java.lang.Object o) { return false; }
+    public boolean removeAll(java.util.Collection c) { return false; }    
+}
+*/
