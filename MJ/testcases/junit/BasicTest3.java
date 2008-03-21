ae7b7fe4076b7613f83522f52e932aab0f5ea413
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-03-21 23:44:05 +0000

    edited GetterSetter testfiles

--- MJ/testcases/junit/BasicTest3.java
+++ MJ/testcases/junit/BasicTest3.java
@@ -9,20 +9,44 @@ public class BasicTest3 extends TestCase {
     GetterSetter<MyBeanClass> gs = new GetterSetter<MyBeanClass>();
 
     @Test public void testGetterSetter() {
-	assertTrue(gs.f1() == 0);
+	assertTrue(gs.getf1() == 0);
 	//	assertTrue(gs.foo() == 0 ); // ERROR
     }
 
     @Test public void testGetterSetter2() {
-	assertTrue(gs.f2() == null);
+	assertTrue(gs.getf2() == null);
     }
-    
+
+    @Test public void testGetterSetter3() {
+	assertTrue(gs.getf3() != null);
+    }
+
+    @Test public void testGetterSetter4() {
+	gs.setf1(3);
+	assertTrue(gs.getf1() == 3);
+    }
+
+    @Test public void testGetterSetter5() {
+	gs.setf2(new HashMap());
+	assertTrue(gs.getf2().get("foo").equals("bar"));
+    }
 }
 
 class MyBeanClass {
     public int f1;
-    protected Object f2;
+    protected HashMap f2;
     private List f3;
     HashMap f4;
+
+    public List getf3() { return new ArrayList(); }
+    public void setf2(HashMap m) {
+	f2 = new HashMap();
+	f2.put("foo", "bar");
+    }
 }
 
+class GetterSetterMyBeanClass extends MyBeanClass {
+    public List getf3() { return super.getf3(); }
+
+    public void setf1(int i) { this.f1 = i; }
+}
