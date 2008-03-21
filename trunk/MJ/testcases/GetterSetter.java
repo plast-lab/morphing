f6edb53c3efc5fe8e1ec8c82dbc0ae8d3272f2b3
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2008-03-21 23:44:05 +0000

    edited GetterSetter testfiles

--- trunk/MJ/testcases/GetterSetter.java
+++ trunk/MJ/testcases/GetterSetter.java
@@ -4,6 +4,17 @@ public class GetterSetter<class X> extends X {
     // TODO: inherit default constructors
     public GetterSetter () { }
 
-    <F>[f] for ( F f : X.fields; no f() : X.methods)
-    public F f () { return f; }
+    <F>[f] for ( F f : X.fields; no get#f() : X.methods)
+    public F get#f () { return f; }
+
+    // unnecessary, but testing out super calls.
+    <F,E*>[f] for ( F f : X.fields; some F get#f() throws E : X.methods)
+    public F get#f () throws E { 
+	return super.get#f(); 
+    }
+
+    <F>[f] for ( F f : X.fields; no set#f(F) : X.methods)
+    public void set#f (F f) { 
+	this.f = f;
+    }
 }
