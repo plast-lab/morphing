6d95ce88b2b4172447fae639fbb95ce3e6421988
freeflygeek <freeflygeek@2f5866ef-7c44-0410-8c2c-03c6f07fd282>
2009-01-23 14:30:10 +0000

    added more Eli's examples

--- trunk/MorphJ/examples/Synchronized.java
+++ trunk/MorphJ/examples/Synchronized.java
@@ -3,12 +3,12 @@ package examples;
 import java.util.*;
 
 class Synchronized<class T> extends T {
-	<R1,A1*,E1*>[m1] for (R1 m1 (A1) throws E1 : T.methods ; no final R1 m1(A1) throws E1 : T.methods)
+	<R1,A1*,E1*>[m1] for (!private R1 m1 (A1) throws E1 : T.methods ; no final R1 m1(A1) throws E1 : T.methods)
 	synchronized public R1 m1(A1 args) throws E1 {
 		return super.m1(args);
 	}
 	
-	<A2*,E2*>[m2] for (void m2 (A2) throws E2 : T.methods ; no final void m2(A2) throws E2 : T.methods)
+	<A2*,E2*>[m2] for (!private void m2 (A2) throws E2 : T.methods ; no final void m2(A2) throws E2 : T.methods)
 	synchronized public void m2(A2 args) throws E2 {
 		super.m2(args);
 	}
